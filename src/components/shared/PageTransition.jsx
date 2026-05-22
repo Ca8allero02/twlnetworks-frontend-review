9ed4f13a853cae43b4e9eea93ee7204739b112e9
valn12 <valentinarrivera03@gmail.com>
2026-05-22 15:43:16 -0500

    cambio transicion

--- src/components/shared/PageTransition.jsx
+++ src/components/shared/PageTransition.jsx
@@ -7,15 +7,22 @@ export default function PageTransition({ children }) {
   const location = useLocation()
   const [playing, setPlaying] = useState(false)
   const [displayLocation, setDisplayLocation] = useState(location)
+  const [showNewPage, setShowNewPage] = useState(true)
 
   useEffect(() => {
     if (location.pathname !== displayLocation.pathname) {
       setPlaying(true)
+      setShowNewPage(false)
+
+      // A los 1.5s cambia la vista (mitad de la animación)
+      setTimeout(() => {
+        setDisplayLocation(location)
+        setShowNewPage(true)
+      }, 1500)
     }
   }, [location])
 
   const handleVideoEnd = () => {
-    setDisplayLocation(location)
     setPlaying(false)
   }
 
@@ -23,6 +30,7 @@ export default function PageTransition({ children }) {
     <>
       {playing && (
         <div className="page-transition__overlay">
+          {!showNewPage && <div className="page-transition__black" />}
           <video
             src={transitionVideo}
             autoPlay
