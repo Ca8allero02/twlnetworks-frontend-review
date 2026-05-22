99e28bc2e850853b0b2257363996fbf21b569feb
valn12 <valentinarrivera03@gmail.com>
2026-05-22 15:38:06 -0500

    act 2 golden feather

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { BrowserRouter, useLocation } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import AppRouter from './router/AppRouter'
+import PageTransition from './components/shared/PageTransition'
+
 function Layout() {
   const location = useLocation()
   const isKanat = location.pathname.startsWith('/kanat')
@@ -14,5 +21,15 @@ function Layout() {
       </PageTransition>
       {!hideLayout && <Footer />}
     </>
+  )
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Layout />
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
